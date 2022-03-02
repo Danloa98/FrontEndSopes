54c6b88153a81c5d4505dccf224b266a28e824a6
unknown <daniedu998@gmail.com>
2022-03-02 12:51:32 -0600

    Docker File and nginx config

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>Hola Sopes1</code> and save to reload.
         </p>
         <a
           className="App-link"
