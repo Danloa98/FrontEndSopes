a75c766d22244e8506631cdab7dc1ab67b7313fe
unknown <daniedu998@gmail.com>
2022-03-10 00:31:13 -0600

    Sockets

--- src/components/Ram.js
+++ src/components/Ram.js
@@ -1,140 +1,91 @@
-import React, { useEffect, useState } from 'react'
-import { Line, defaults } from 'react-chartjs-2'
+import React, { useEffect, useState } from "react";
+import { Line, defaults } from "react-chartjs-2";
+import io from 'socket.io-client';
 
-
-import {
-  Rectangulo,
-  Rectangulo2,
-  Contenedor
-} from './NavBarElements';
+import { Rectangulo, Rectangulo2, Contenedor } from "./NavBarElements";
 //import { Line, defaults } from 'react-chartjs-2'
 
-defaults.global.tooltips.enabled = false
-defaults.global.legend.position = 'bottom'
-const baseUrl='http://localhost:5000';
-
-
+defaults.global.tooltips.enabled = false;
+defaults.global.legend.position = "bottom";
+const baseUrl = "https://loyal-operation-341718.uc.r.appspot.com";
 
 const Ram = () => {
-
-//-------------------
-const [operaciones,setOperations] = useState([])
-useEffect(() => {
-  getOperations();
-}, [])
-
-const getOperations = async() => {
-  await fetch(`${baseUrl}/api/data`, {
-      method: 'GET',
-      headers: {
-          "Content-Type": "application/json"
-      }
-  })
-  .then(resp => resp.json())
-  .then(data => {
-    setOperations(data)
-    console.log("wwwwwwwwwwwww")
-
-  }).catch(console.error)
-}
-
-const tempInt=operaciones.map( ({Fecha, CO2, Iluminacion, Humedad, TemperaturaInterna,TemperaturaExterna}, i) => {
-  return (
-      
-
-          TemperaturaInterna
-
-      
-  )
-})
+  const socket = io.connect(baseUrl);
+  //-------------------
+  useEffect(() => {
+    socket.emit("ram", "asd-prueba");    
+    socket.on("ram", async (mensaje) => {
+    console.log("MENSAJE: ", mensaje);
+    })
+  }, [socket]);
+  const tempInt = []
+  console.log(tempInt);
 
 
-console.log(tempInt)
-
-//------------------
-
-
-
+  //------------------
 
   return (
     <div>
       <div>RAM</div>
       <Rectangulo>
         <h3>VM1</h3>
-        <div className='rectangle2'>
+        <div className="rectangle2">
           [Total RAM]
           {tempInt[0]}
         </div>
-        <div className='rectangle2'>
-          RAM en uso
-        </div>
-        <div className='rectangle2'>
-          RAM en uso % 
-        </div>
-        <div className='rectangle2'>
-          RAM libre
-        </div>
+        <div className="rectangle2">RAM en uso</div>
+        <div className="rectangle2">RAM en uso %</div>
+        <div className="rectangle2">RAM libre</div>
       </Rectangulo>
       <Rectangulo2>
         <h3>VM2</h3>
-        <div className='rectangle2'>
-          Total RAM
-        </div>
-        <div className='rectangle2'>
-          RAM en uso
-        </div>
-        <div className='rectangle2'>
-          RAM en uso %
-        </div>
-        <div className='rectangle2'>
-          RAM libre
-        </div>
+        <div className="rectangle2">Total RAM</div>
+        <div className="rectangle2">RAM en uso</div>
+        <div className="rectangle2">RAM en uso %</div>
+        <div className="rectangle2">RAM libre</div>
       </Rectangulo2>
 
       <Contenedor>
-      <Line
-        data={{
-          labels: [1,2,3,4],
-          datasets: [
-            {
-              label: 'VM1',
-              data:tempInt ,
-              borderColor: 'orange',
-              borderWidth: 1,
-            },
-             {
-               label: 'VM2',
-               data: tempInt,
-               borderColor: 'red',
-               
-             },
-          ],
-        }}
-        height={400}
-        width={600}
-        options={{
-          maintainAspectRatio: false,
-          scales: {
-            yAxes: [
+        <Line
+          data={{
+            labels: [1, 2, 3, 4],
+            datasets: [
               {
-                ticks: {
-                  beginAtZero: true,
-                },
+                label: "VM1",
+                data: tempInt,
+                borderColor: "orange",
+                borderWidth: 1,
+              },
+              {
+                label: "VM2",
+                data: tempInt,
+                borderColor: "red",
               },
             ],
-          },
-          legend: {
-            labels: {
-              fontSize: 25,
+          }}
+          height={400}
+          width={600}
+          options={{
+            maintainAspectRatio: false,
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    beginAtZero: true,
+                  },
+                },
+              ],
             },
-          },
-        }}
-      />
+            legend: {
+              labels: {
+                fontSize: 25,
+              },
+            },
+          }}
+        />
       </Contenedor>
-
-
     </div>
-  )
-}
+  );
+};
 
-export default Ram
+export default Ram;
